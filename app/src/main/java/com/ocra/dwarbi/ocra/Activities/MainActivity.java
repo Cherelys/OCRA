e56a3b6675dd83e8d06e3c26bfff5ed8a2acc91c
Cherelys Dwarka Sing <cherdwar@gmail.com>
2019-03-14 09:23:19 -0300

    Update

--- app/src/main/java/com/ocra/dwarbi/ocra/Activities/MainActivity.java
+++ app/src/main/java/com/ocra/dwarbi/ocra/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.ocra.dwarbi.ocra.Activities;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
